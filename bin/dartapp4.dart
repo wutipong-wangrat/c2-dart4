1a8087895db52511972a3ee71523e3925dbd3695
Ddvrg <bigbosszaa086@gmail.com>
2021-11-22 09:38:20 +0700

    initial commit

---
+++ bin/dartapp4.dart
@@ -0,0 +1,5 @@
+import 'package:dartapp4/dartapp4.dart' as dartapp4;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dartapp4.calculate()}!');
+}
---
+++ test/dartapp4_test.dart
@@ -0,0 +1,8 @@
+import 'package:dartapp4/dartapp4.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
