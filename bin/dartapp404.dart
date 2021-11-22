6491a91ab76f4ebe57a2743ea15e14de1db14e32
Ddvrg <bigbosszaa086@gmail.com>
2021-11-22 12:11:49 +0700

    initial commit

--- bin/dartapp404.dart
+++ bin/dartapp404.dart
@@ -1,9 +1,13 @@
-void main() {
-  List<int> number = [10, 20, 30];
+void main(List<String> args) {
+  List<int> number = [1, 2, 3];
 
   var count = number.length;
   print(count);
-  for (var member in number) {
-    print(member);
+
+  for (var a = 0; a < 3; a++) {
+    print(number[a]);
   }
+  // for (var member in number) {
+  //   print(member);
+  // }
 }
---
+++ bin/dartapp405.dart
@@ -0,0 +1,9 @@
+void main() {
+  List<int> number = [10, 20, 30];
+
+  var count = number.length;
+  print(count);
+  for (var member in number) {
+    print(member);
+  }
+}
